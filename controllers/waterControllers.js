118e5aac71fa13aa7325f1f3ce329faa03223f09
artanton <kabletrade@gmail.com>
2024-04-17 20:44:23 +0300

    water-add2

--- controllers/waterControllers.js
+++ controllers/waterControllers.js
@@ -52,6 +52,46 @@ const addWater = async (req, res) => {
   }
 };
 
+const today = async (req, res) => {
+  const { id } = req.params;
+  const {_id: owner}=req.user;
+  const result = await Water.findOne({_id:id, owner});
+  if (!result) {
+    throw HttpError(404, `Not found`);
+  }
+
+  res.json(result);
+};
+
+const deleteWaterDose = async (req, res) => {
+  const { id } = req.params;
+  const { _id: owner } = req.user;
+  const dateWithoutTime = new Date().toISOString().substring(0, 10);
+  const ToDayWaterData = await Water.findOne({ owner, date: dateWithoutTime });
+
+  if (!ToDayWaterData) {
+    return res.status(404).json({ error: 'Data not found' });
+  }
+
+  const updatedWaterData = await Water.findOneAndUpdate(
+     {'dailyWaterInfo._id': id },
+      {
+        $inc: { waterTotal: -waterDose},
+        $pull: { dailyWaterInfo: { _id: id }},
+        $set: { 
+          persantRate: Math.round((waterTotal + waterDose) / userDailyNorma * 100)}
+        
+      },
+    { new: true }
+  );
+
+  
+  res.json({ message: 'Delete success' });
+};
+
+
 export default {
   addWater: ctrlWrapper(addWater),
+  today: ctrlWrapper(today),
+  deleteWaterDose: ctrlWrapper(deleteWaterDose),
 };
