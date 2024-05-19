93f4190ad6a69f733e94957ebcf778f20dc91d77
Anton Pedan <77937504+snap515@users.noreply.github.com>
2024-05-19 14:32:04 +0200

    bugfix with reponse of addWater

--- controllers/waterControllers.js
+++ controllers/waterControllers.js
@@ -35,7 +35,21 @@ const addWater = async (req, res) => {
   } else {
     const { _id: id, waterTotal, waterSavings } = toDayWaterData;
 
-    const result = await Water.findOneAndUpdate(
+    // const result = await Water.findOneAndUpdate(
+    //   { _id: id },
+    //   {
+    //     $push: { waterNotes: { createdDate: date, waterDose } },
+    //     $inc: { waterTotal: +waterDose, waterSavings: 1 },
+    //     $set: {
+    //       persantRate: Math.round(
+    //         ((waterTotal + waterDose) / userDailyNorma) * 100
+    //       ),
+    //     },
+    //   },
+    //   { new: true }
+    // );
+
+    const updatedData = await Water.findOneAndUpdate(
       { _id: id },
       {
         $push: { waterNotes: { createdDate: date, waterDose } },
@@ -48,9 +62,11 @@ const addWater = async (req, res) => {
       },
       { new: true }
     );
-    const response = { ...result, lastWaterDose: req.body };
 
-    res.status(200).json(response);
+    const result = updatedData.toObject();
+    result.lastWaterDose = req.body;
+
+    res.status(200).json(result);
   }
 };
 
